49ff92c148f598191ea45cd00f244b6983395db1
mfa2001 <39986028+mfa2001@users.noreply.github.com>
2022-02-17 15:41:17 +0100

    Add first lines

---
+++ client.c
@@ -0,0 +1,20 @@
+//Server file
+
+typedef struct ConfigFile
+{
+    /* data */
+    Port;
+    void Construct("Data"){
+
+    }
+};
+
+
+void read(char file[]){
+
+}
+
+void main(){
+    char file[] = "ClientConfigFile";
+    read(file)
+}
---
+++ server.py
@@ -0,0 +1,19 @@
+#Client File
+import sys
+
+class ConfigFile():
+    def __init__(self) -> None:
+        #Diferent Config statments
+        pass
+
+
+def read(file):
+    for line in file:
+        
+    #Read operation from file
+    configuration = ConfigFile("Add diferent config statments")
+    pass
+
+if __name__ == "__main__":
+    file = "server.cfg"
+    read(file)
