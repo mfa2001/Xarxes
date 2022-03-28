a4b1cfeeed3a2a79020e560f1e36d08b456d1b29
Marc Farran <mfarran01@gmail.com>
2022-03-28 16:52:35 +0200

    Sockets mounted

--- Xarxes/server/server.py
+++ Xarxes/server/server.py
@@ -1,7 +1,25 @@
 # Server File
 import sys
+import socket
+
+server_sockets = None
+bbdd_clients = []
 serverConfigure = None
 debug = False
+
+class Socket:
+    def __init__(self):
+        self.udp_socket = None
+
+        self.tcp_socket = None
+
+class Client:
+    def __init__(self):
+        self.id = None
+        self.state = "DISCONNECTED"
+        self.udp_port = None
+        self.tcp_port = None
+        self.address = None
 
 class ConfigFile:
     def __init__(self, ID, udpPort, tcpPort) -> None:
@@ -43,10 +61,39 @@ def parseArg():
     global serverConfigure
     serverConfigure = readFromFile(file)
 
-def start():
-    pass
+def readClients():
+    global bbdd_clients
+    file_clients = open("bbdd_dev.dat","r")
+    for line in file_clients:
+        if line != '\n':
+            valid_client = Client()
+            valid_client.id = line
+            bbdd_clients.append(valid_client)
+    file_clients.close()
+    if debug:
+        print("Read clients correctly")
+
+def openUdpTcpSocket():
+    global server_sockets
+    server_sockets = Socket()
+    server_sockets.udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    server_sockets.udp_socket.bind(("",int(serverConfigure.UDP_port)))
+
+    server_sockets.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server_sockets.tcp_socket.bind(("",int(serverConfigure.TCP_port)))
+    server_sockets.tcp_socket.listen(5)
+
+def udpService():
+    global server_sockets
+    server_sockets: Socket
+    if debug:
+        print("UDP socket waiting")
+    while True:
+        #server_sockets.udp_socket
 
 
 if __name__ == "__main__":
     parseArg()
-    start()
+    readClients()
+    openUdpTcpSocket()
+    udpService()
